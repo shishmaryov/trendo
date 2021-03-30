7052f2dbe9d9f840699d68b73c711c2315393103
Konstantin <shishmaryovkonstantin@gmail.com>
2021-03-30 22:46:56 +0300

    responsive

---
+++ build/js/script.js
@@ -0,0 +1,158 @@
+$(document).ready(function(){
+	$('.faq__question--1').click(function(){
+		$('.faq__open--1').toggleClass('faq__open--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--1').click(function(){
+		$('.faq__question--1').toggleClass('faq__question--1-active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--2').click(function(){
+		$('.faq__open--2').toggleClass('faq__open--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--2').click(function(){
+		$('.faq__question--2').toggleClass('faq__question--1-active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--3').click(function(){
+		$('.faq__open--3').toggleClass('faq__open--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--4').click(function(){
+		$('.faq__open--4').toggleClass('faq__open--active'); return false;
+	});
+});
+
+
+$(document).ready(function(){
+	$('.faq__question--4').click(function(){
+		$('.faq__question--4').toggleClass('faq__question--4-active'); return false;
+	});
+});
+
+
+
+$(document).ready(function(){
+	$('.faq__question--5').click(function(){
+		$('.faq__open--5').toggleClass('faq__open--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--5').click(function(){
+		$('.faq__question--5').toggleClass('faq__question--5-active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--6').click(function(){
+		$('.faq__open--6').toggleClass('faq__open--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--6').click(function(){
+		$('.faq__question--6').toggleClass('faq__question--6-active'); return false;
+	});
+});
+
+
+$(document).ready(function(){
+	$('.faq__question--7').click(function(){
+		$('.faq__open--7').toggleClass('faq__open--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--7').click(function(){
+		$('.faq__question--7').toggleClass('faq__question--7-active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--8').click(function(){
+		$('.faq__open--8').toggleClass('faq__open--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--8').click(function(){
+		$('.faq__question--8').toggleClass('faq__question--8-active'); return false;
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+	$('.faq__question--1').click(function(){
+		$('.faq__question--1').toggleClass('faq__question--1-active'); return false;
+	});
+});
+
+
+
+
+
+$(document).ready(function(){
+	$('.faq__question--1').click(function(){
+		$('.faq__answer--1').toggleClass('faq__answer--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--2').click(function(){
+		$('.faq__answer--2').toggleClass('faq__answer--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--3').click(function(){
+		$('.faq__answer--3').toggleClass('faq__answer--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--4').click(function(){
+		$('.faq__answer--4').toggleClass('faq__answer--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--5').click(function(){
+		$('.faq__answer--5').toggleClass('faq__answer--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--6').click(function(){
+		$('.faq__answer--6').toggleClass('faq__answer--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--7').click(function(){
+		$('.faq__answer--7').toggleClass('faq__answer--active'); return false;
+	});
+});
