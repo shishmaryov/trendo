ab3e3b6b93e54cf10775e036d0d625d19b275220
Konstantin <shishmaryovkonstantin@gmail.com>
2021-03-31 11:18:21 +0300

    FAQ

--- build/js/script.js
+++ build/js/script.js
@@ -1,120 +1,91 @@
+// FAQ open
 $(document).ready(function(){
 	$('.faq__question--1').click(function(){
-		$('.faq__open--1').toggleClass('faq__open--active'); return false;
+		$('.faq__open--1').addClass('faq__open-1--active'); return false;
 	});
 });
 
 $(document).ready(function(){
 	$('.faq__question--1').click(function(){
-		$('.faq__question--1').toggleClass('faq__question--1-active'); return false;
+		$('.faq__open-1--active').toggleClass('faq__open--close'); return false;
 	});
 });
 
 $(document).ready(function(){
 	$('.faq__question--2').click(function(){
-		$('.faq__open--2').toggleClass('faq__open--active'); return false;
+		$('.faq__open--2').addClass('faq__open-2--active'); return false;
 	});
 });
 
 $(document).ready(function(){
 	$('.faq__question--2').click(function(){
-		$('.faq__question--2').toggleClass('faq__question--1-active'); return false;
+		$('.faq__open-2--active').toggleClass('faq__open--close'); return false;
 	});
 });
 
 $(document).ready(function(){
 	$('.faq__question--3').click(function(){
-		$('.faq__open--3').toggleClass('faq__open--active'); return false;
+		$('.faq__open--3').addClass('faq__open-3--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--3').click(function(){
+		$('.faq__open-3--active').toggleClass('faq__open--close'); return false;
 	});
 });
 
 $(document).ready(function(){
 	$('.faq__question--4').click(function(){
-		$('.faq__open--4').toggleClass('faq__open--active'); return false;
+		$('.faq__open--4').addClass('faq__open-4--active'); return false;
 	});
 });
-
 
 $(document).ready(function(){
 	$('.faq__question--4').click(function(){
-		$('.faq__question--4').toggleClass('faq__question--4-active'); return false;
-	});
-});
-
-
-
-$(document).ready(function(){
-	$('.faq__question--5').click(function(){
-		$('.faq__open--5').toggleClass('faq__open--active'); return false;
+		$('.faq__open-4--active').toggleClass('faq__open--close'); return false;
 	});
 });
 
 $(document).ready(function(){
 	$('.faq__question--5').click(function(){
-		$('.faq__question--5').toggleClass('faq__question--5-active'); return false;
+		$('.faq__open--5').addClass('faq__open-5--active'); return false;
+	});
+});
+
+$(document).ready(function(){
+	$('.faq__question--5').click(function(){
+		$('.faq__open-5--active').toggleClass('faq__open--close'); return false;
 	});
 });
 
 $(document).ready(function(){
 	$('.faq__question--6').click(function(){
-		$('.faq__open--6').toggleClass('faq__open--active'); return false;
+		$('.faq__open--6').addClass('faq__open-6--active'); return false;
 	});
 });
 
 $(document).ready(function(){
 	$('.faq__question--6').click(function(){
-		$('.faq__question--6').toggleClass('faq__question--6-active'); return false;
-	});
-});
-
-
-$(document).ready(function(){
-	$('.faq__question--7').click(function(){
-		$('.faq__open--7').toggleClass('faq__open--active'); return false;
+		$('.faq__open-6--active').toggleClass('faq__open--close'); return false;
 	});
 });
 
 $(document).ready(function(){
 	$('.faq__question--7').click(function(){
-		$('.faq__question--7').toggleClass('faq__question--7-active'); return false;
+		$('.faq__open--7').addClass('faq__open-7--active'); return false;
 	});
 });
 
 $(document).ready(function(){
-	$('.faq__question--8').click(function(){
-		$('.faq__open--8').toggleClass('faq__open--active'); return false;
-	});
-});
-
-$(document).ready(function(){
-	$('.faq__question--8').click(function(){
-		$('.faq__question--8').toggleClass('faq__question--8-active'); return false;
+	$('.faq__question--7').click(function(){
+		$('.faq__open-7--active').toggleClass('faq__open--close'); return false;
 	});
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function(){
-	$('.faq__question--1').click(function(){
-		$('.faq__question--1').toggleClass('faq__question--1-active'); return false;
-	});
-});
-
-
-
-
-
+// FAQ answer
 $(document).ready(function(){
 	$('.faq__question--1').click(function(){
 		$('.faq__answer--1').toggleClass('faq__answer--active'); return false;
